a95d4548a89b0097d3675e35a9659a84fc09709f
jnhenkel <jeffnhenkel@gmail.com>
2022-01-27 21:38:48 -0700

    question8

--- practicum/week-2/question8.js
+++ practicum/week-2/question8.js
@@ -8,5 +8,18 @@ output: [1, 0, 1, 2]
 */
 
 var biggerAndEven = function(input) {
-
+    let result = [];
+    for (let i=0; i<input.length; i++) {
+        let tempSum = 0;
+        let num = input[i];
+        let tempArray = input.map((x) => x); /*copy not reference*/
+        tempArray.splice(i,1); /*removes 1 element at position i*/
+        for (let el of tempArray) {
+            if (el>num && el%2==0){
+                tempSum++;
+            }
+        }
+        result.push(tempSum);
+    }
+    return result;
 };
